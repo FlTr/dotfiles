901a49084853d7fa4997e4c984021047ed4a8338
EdVanDance <EdVanDance@gmail.com>
2013-06-25 16:44:44 +0200

    zsh: enhance completion
    * With correction.
    * With colors.
    * With interactive selection for long list of alternatives.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,3 +1,9 @@
+# Use modern completion system
+autoload -Uz compinit
+compinit
+
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
@@ -6,3 +12,12 @@ zstyle ':completion:*' insert-tab pending
 
 # complete 'cd ..<TAB>' to 'cd ../'
 zstyle -e ':completion:*' special-dirs '[[ $PREFIX = (../)#(|.|..) ]] && reply=(..)'
+
+# complete with colors
+eval "$(dircolors -b)"
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+
+# enable interactive selection (after 3rd tab)
+zstyle ':completion:*' menu select=long
+zstyle ':completion:*' select-prompt %p%s
