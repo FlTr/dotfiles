7598fb2232cbae401d098c16d3ffad43c7e752d7
EdVanDance <EdVanDance@gmail.com>
2013-08-26 10:48:22 +0200

    ruby: add rvm completion

--- ruby/rvm.zsh
+++ ruby/rvm.zsh
@@ -1,2 +1,3 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 path=($HOME/.rvm/bin "$path[@]") # add RVM to PATH for scripting at beginning
+[[ -d "$HOME/.rvm/scripts/zsh/Completion" ]] && fpath=("$HOME/.rvm/scripts/zsh/Completion" $fpath)
