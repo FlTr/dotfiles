6147f8ac28fa7c93f62a507b869b008024082fb1
EdVanDance <EdVanDance@gmail.com>
2013-09-04 14:02:35 +0200

    zsh: fix DEL key for st

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -47,5 +47,6 @@ bindkey '^[[H' beginning-of-line
 bindkey '^[[4~' end-of-line
 bindkey '^[[F' end-of-line
 bindkey '^[[3~' delete-char
+bindkey '^[[P' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
