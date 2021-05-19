c3531f4d22c6f7b34c8198419f3d2f1e545b415c
EdVanDance <EdVanDance@gmail.com>
2021-05-19 20:59:38 +0200

    git: cleanup aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,12 +1,3 @@
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-#  alias git=$hub_path
-fi
-
-# The rest of my fun git aliases
 alias g=git
 alias ga='git add'
 alias gl='git pull --prune'
@@ -15,14 +6,13 @@ alias gp='git push'
 alias gd='git diff -M'
 alias gds='git diff -M --staged'
 alias gdw='git diff -M --word-diff=color'
-alias gdt='git difftool --dir-diff' # --dir-diff needs git >= 1.7.11
-alias gc='git commit'
-alias gcv='git commit -v'
-alias gca='git commit -a'
-alias gcav='git commit -av'
+alias gdt='git difftool --dir-diff'
+alias gc='git commit -v'
+alias gca='git commit -av'
 alias gco='git checkout'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gs='git status -sb'
+alias gsu='git submodule update'
 alias gbak='git push --mirror backup'
 
 # git-svn aliases
