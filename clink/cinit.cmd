96161f1d86eda0b51e909037ff707f814f7711cb
EdVanDance <EdVanDance@gmail.com>
2021-06-02 13:08:19 +0200

    win: refreshenv to the rescue

--- clink/cinit.cmd
+++ clink/cinit.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+if [%1] EQU [--path-only] goto path_only
+
 :: Start the goodness
 call clink inject --quiet
 
@@ -9,16 +11,17 @@ if not exist %DOT%\clink\aliases (
 )
 doskey /macrofile=%DOT%\clink\aliases
 
-:: Setup PATH
-if exist %USERPROFILE%\bin\ set PATH=%USERPROFILE%\bin;%PATH%
-set PATH=%DOT%\win\bin;%PATH%
-for /F %%p in ('scoop prefix z.lua') do set PATH=%PATH%;%%p
-
 :: ENV
 set _ZL_NO_CHECK=1
 set FZF_DEFAULT_COMMAND=fd -t f
 set FZF_DEFAULT_OPTS=--height=40%% --layout=reverse --bind "alt-e:execute(nvr {})"
 
-
 :: Clean title
 title cmd
+
+:: Setup PATH
+:path_only
+if exist %USERPROFILE%\bin\ set PATH=%USERPROFILE%\bin;%PATH%
+set PATH=%DOT%\win\bin;%PATH%
+for /F %%p in ('scoop prefix z.lua') do set PATH=%PATH%;%%p
+
